fea15b1d19171c4ff9d6da4fe0bbdff273f95dd4
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-16 13:00:57 -0700

    add UI functionality

--- NumbersToWords/Program.cs
+++ NumbersToWords/Program.cs
@@ -1,10 +1,29 @@
 using System;
-using System.Collections.Generic; //remove if not using Dictionary in app //
+using NumbersToWords;
 
 class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("Input the number to conversion: ");
+    string userInput = Console.ReadLine();
+    int number;
+    if(int.TryParse(userInput, out number))
+    {
+      Console.WriteLine("Your number in words is: ");
+      if(number < 0)
+      {
+        Console.WriteLine("negative "+Conversion.NumberToStringConversion(number));
+      }
+      else
+      {
+        Console.WriteLine(Conversion.NumberToStringConversion(number));
+      }
+    }
+    else
+    {
+      Console.WriteLine("Incorrect input, only integers numbers");
+    }
+    Main();
   }
 }
